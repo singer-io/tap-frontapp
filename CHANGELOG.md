a9be2e6e77b6aafe0fa48f25559824414c7639da
Andy Lu <andy@stitchdata.com>
2018-12-17 16:47:04 +0000

    Bump to 1.0.0, and requests to 2.20.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 # Changelog
 
+## 1.0.0
+  * General release of the tap
+
+## 0.3.2
+  * Update version of `requests` to `0.20.0` in response to CVE 2018-18074
+
 ## 0.3.1
   * Fixes a memory issue when syncing contacts [#4](https://github.com/singer-io/tap-emarsys/pull/4)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-frontapp",
-    version="0.3.1",
+    version="1.0.0",
     description="Singer.io tap for extracting data from the FrontApp API",
     author="bytcode.io",
     url="http://singer.io",
@@ -14,7 +14,7 @@ setup(
         "pendulum",
         "ratelimit",
         "backoff",
-        "requests",
+        "requests==2.20.0",
     ],
     entry_points="""
     [console_scripts]
