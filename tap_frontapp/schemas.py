7c87ec2a0a0bc4d450623ba1322e7e2506d67f6e
Pierre de Poulpiquet <pdebrescanvel@gmail.com>
2020-07-15 20:59:10 +0200

    Add analytics tables streams (#12)
    * Add 'tags_table' stream
    
    As per requested in issue #11 , this add a stream for the "tags_table"
    analytic table of frontapp.
    
    * Fix schema
    
    The types of metrics were not properly defined.
    
    * Add all remaining analytics table
    
    Add support for:
    - customers_table
    - first_response_histo
    - resolution_histo
    - response_histo
    - top_conversations_table
    - top_reaction_time_table
    - top_replies_table
    
    * Fix typo
    
    A typo introduced itself in the code...
    
    * Fix type in customer_table schema
    
    The resource_url wasn't properly typed.
    
    * Fix STATE message
    
    The stream were being reseted before sync'ing the next ones which
    leads to STATE messages being incomplete -> only the last synced stream
    had the proper date_to_resume.
    
    Co-authored-by: Pierre de Poulpiquet <pdepoulpiquet@ns342282.ip-188-165-192.eu>

--- tap_frontapp/schemas.py
+++ tap_frontapp/schemas.py
@@ -6,13 +6,37 @@ from singer import utils
 
 class IDS(object): # pylint: disable=too-few-public-methods
     TEAM_TABLE = 'team_table'
+    TAGS_TABLE = 'tags_table'
+    CUSTOMERS_TABLE = 'customers_table'
+    FIRST_RESPONSE_HISTO = 'first_response_histo'
+    RESOLUTION_HISTO = 'resolution_histo'
+    RESPONSE_HISTO = 'response_histo'
+    TOP_CONVERSATION_TABLE = 'top_conversations_table'
+    TOP_REACTION_TIME_TABLE = 'top_reaction_time_table'
+    TOP_REPLIES_TABLE = 'top_replies_table'
 
 STATIC_SCHEMA_STREAM_IDS = [
-    IDS.TEAM_TABLE
+    IDS.TEAM_TABLE,
+    IDS.TAGS_TABLE,
+    IDS.CUSTOMERS_TABLE,
+    IDS.FIRST_RESPONSE_HISTO,
+    IDS.RESOLUTION_HISTO,
+    IDS.RESPONSE_HISTO,
+    IDS.TOP_CONVERSATION_TABLE,
+    IDS.TOP_REACTION_TIME_TABLE,
+    IDS.TOP_REPLIES_TABLE
 ]
 
 PK_FIELDS = {
-    IDS.TEAM_TABLE: ['analytics_date', 'analytics_range', 'teammate_v']
+    IDS.TEAM_TABLE: ['analytics_date', 'analytics_range', 'teammate_v'],
+    IDS.TAGS_TABLE: ['analytics_date', 'analytics_range', 'tag_v'],
+    IDS.CUSTOMERS_TABLE: ['analytics_date', 'analytics_range', 'resource_t', 'resource_v'],
+    IDS.FIRST_RESPONSE_HISTO: ['analytics_date', 'analytics_range', 'time_v'],
+    IDS.RESOLUTION_HISTO: ['analytics_date', 'analytics_range', 'time_v'],
+    IDS.RESPONSE_HISTO: ['analytics_date', 'analytics_range', 'time_v'],
+    IDS.TOP_CONVERSATION_TABLE: ['analytics_date', 'analytics_range', 'teammate_v'],
+    IDS.TOP_REACTION_TIME_TABLE: ['analytics_date', 'analytics_range', 'teammate_v'],
+    IDS.TOP_REPLIES_TABLE: ['analytics_date', 'analytics_range', 'teammate_v']
 }
 
 def normalize_fieldname(fieldname):
