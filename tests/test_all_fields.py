dbefbaf5eb089d6968d1c2dada91f29619bfa729
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-05-21 11:36:02 +0530

    add missing fields

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -3,9 +3,37 @@ from base import FrontAppBaseTest
 from tap_tester.base_suite_tests.all_fields_test import AllFieldsTest
 
 # Fields that exist in the schema but may not be returned by the FrontApp API
-# in all test environments. Populate after a first real test run if needed.
+# in all test environments. The Front API only returns metrics that have data
+# for a given entity, so metric fields are conditionally absent from records.
+_METRIC_FIELDS = {
+    "avg_first_response_time",
+    "avg_handle_time",
+    "avg_response_time",
+    "avg_sla_breach_time",
+    "avg_total_reply_time",
+    "new_segments_count",
+    "num_active_segments_full",
+    "num_archived_segments",
+    "num_archived_segments_with_reply",
+    "num_csat_survey_response",
+    "num_messages_received",
+    "num_messages_sent",
+    "num_sla_breach",
+    "pct_csat_survey_satisfaction",
+    "pct_tagged_conversations",
+    "num_open_segments_start",
+    "num_closed_segments",
+    "num_open_segments_end",
+    "num_workload_segments",
+}
+
 KNOWN_MISSING_FIELDS = {
-    # "<stream_name>": {"<field_name>"},
+    "accounts_table": _METRIC_FIELDS,
+    "channels_table": _METRIC_FIELDS,
+    "inboxes_table": _METRIC_FIELDS,
+    "tags_table": _METRIC_FIELDS,
+    "teammates_table": _METRIC_FIELDS,
+    "teams_table": _METRIC_FIELDS,
 }
 
 
